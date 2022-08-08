f403900d04fe23727be06e0e06568b131f37c26a
faizalajiw <faizalajiwibowo7@gmail.com>
2022-08-08 21:48:53 +0700

    edit names

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
       <div className="grid grid-cols-2 gap-14">
         {/* grid 1 */}
         <div className="py-24">
-          <img alt="ilustration-SatsetVPN" src={Illustration1} />
+          <img alt="ilustration-SatSetVPN" src={Illustration1} />
         </div>
         {/* end grid 1 */}
         {/* grid 2 */}
@@ -15,11 +15,11 @@ const Banner = () => {
           <div className="text-5xl font-medium text-dark-blue">
             Want anything to be
             <br />
-            easy with <strong>SatsetVPN.</strong>
+            easy with <strong>SatSetVPN.</strong>
           </div>
           <div className="text-lg font-normal mt-9 mb-12">
             Provide a network for all your needs with ease and fun using{" "}
-            <strong>SatsetVPN</strong> {" "}
+            <strong>SatSetVPN</strong> {" "}
             discover interesting features from us.
           </div>
           <button className="px-16 py-4 bg-red-500 rounded-xl text-white text-base font-bold drop-shadow-xl">
